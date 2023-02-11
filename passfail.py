023dc39f571bd636ba61dba3dd4a7bc39a6c3ef0
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2023-02-11 17:44:26 +0530

    Add files via upload

---
+++ passfail.py
@@ -0,0 +1,9 @@
+m1=int(input("enter mark 1 :"))
+m2=int(input("enter mark 2 :"))
+
+if m1 >= 40  and m2 >= 40:
+    print ("pass")
+elif m1 <40 and m2 <40:
+    print("fail")
+else :
+    print ("atkt")
