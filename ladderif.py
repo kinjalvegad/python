479d69de305fe24b451fbaf15afb50931e7857cc
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2023-02-11 17:33:15 +0530

    Add files via upload

---
+++ ladderif.py
@@ -0,0 +1,7 @@
+course=input("enter your course:")
+if course=="pgdca":
+    print("ok")
+elif course=="bca":
+        print("ok")
+else:
+    print("not ok")
---
+++ nested.py
@@ -0,0 +1,9 @@
+pgdca="pass"
+per =85
+if pgdca=="pass":
+    if per>=70:
+        print("eligible")
+    else:
+        print("not eligible")
+else:
+    print("not eligible")
